c5f0c5236bc22e9157b862a76cf5db9caf70d88a
cd <corentin-d@corentin-d.com>
2018-10-14 22:08:57 +0200

    Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   public title: string = 'Working with branches';
+
+  public secondes: number;
+  private counterSubscription: Subscription;
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+
+    const counter = interval(1000);
+    this.counterSubscription = counter.subscribe(
+      (value) => {
+        this.secondes = value;
+      },
+      (error) => {
+        console.log('erreur suivante:' + error)
+      },
+      () => {
+        console.log('Observable complete')
+      }
+    )
+
+  }
+
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
+  }
 
 
 
